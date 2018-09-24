3ee8297881bfcbafb167dfcc52b5120b61e2e387
Alfian Losari <alfian@losari.org>
2018-09-24 10:09:22 +0700

    Initial Commit

---
+++ CoreDataRemote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataRemote
+//
+//  Created by Alfian Losari on 24/09/18.
+//  Copyright © 2018 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
